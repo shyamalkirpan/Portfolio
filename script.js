1e90dcc3e88d87fb924e53568d8958f7c660c831
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2022-02-17 18:51:09 +0530

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+  var adobeDCView = new AdobeDC.View({
+    clientId: "36f72b21a13e43788488ddaccb3ff323",
+    divId: "adobe-dc-view",
+  });
+  adobeDCView.previewFile(
+    {
+      content: {
+        location: {
+          url: "https://shyamalkirpan.github.io/Portfolio/ShyamalResume.pdf",
+        },
+      },
+      metaData: { fileName: "ShyamalResume.pdf" },
+    },
+    {
+      showAnnotationTools: false,
+      showLeftHandPanel: false,
+      showPageControls: false,
+    }
+  );
+});
